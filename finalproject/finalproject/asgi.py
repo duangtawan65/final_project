a140645191ad7e823c5f36f8bba1abfeb5be3e34
duangtawan65 <duangtawan.si.65@ubu.ac.th>
2024-12-26 01:12:36 +0700

    final project

--- finalproject/finalproject/asgi.py
+++ finalproject/finalproject/asgi.py
@@ -10,12 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 # finalproject/asgi.py
 import os
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalproject.settings')
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter , URLRouter
 from chat import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finalproject.settings')
+
 
 
 application = ProtocolTypeRouter(
