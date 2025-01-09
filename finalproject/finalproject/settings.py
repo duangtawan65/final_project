9a490078b5f583bf4a2b9b04ff77b0e6963e2a39
duangtawan65 <duangtawan.si.65@ubu.ac.th>
2025-01-09 11:08:01 +0700

    final project

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     },
     'sqlite_db': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',  
+            'NAME': BASE_DIR / 'db.sqlite3',
     },
 }
 
